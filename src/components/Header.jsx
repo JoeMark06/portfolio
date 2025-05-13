63bf497fa085dced4bcfedf1f53d58a9cee7fb97
JoeMark06 <130420458+JoeMark06@users.noreply.github.com>
2025-05-13 13:06:46 +0800

    Deploy application to Netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
       </div>
       <div className="w-1/3 flex justify-center">
         <img
-          src="/dist/assets/image.png" // Replace with your image path
+          src="/dist/assets/me.png" // Replace with your image path
           alt="Joe Mark B. Tolentino"
           className="rounded-full w-full h-auto max-w-xs object-cover"
         />
